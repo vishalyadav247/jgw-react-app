28eacba2fa04894c432a7b93b0d2a1cf1ebb1248
vishal <vishalyadav247@gmail.com>
2023-03-25 01:20:40 +0530

    namming error restoration

--- src/components/AgencyServices.js
+++ src/components/AgencyServices.js
@@ -1,6 +1,6 @@
 import { Box, CardMedia } from '@mui/material'
 import React from 'react'
-import Webdesign from '../images/Webdesign.jpg'
+
 
 
 
@@ -8,16 +8,16 @@ export default function AgencyServices() {
     return (
         <Box className="pageWidth" sx={{display:"flex",flexWrap:"wrap"}}>
             <Box sx={{width:{xs:"100%",sm:"50%",md:"25%"}}}>
-                <CardMedia src={Webdesign} sx={{ display:{xs:"none",md:"block"} }} alt="Website & Design" />
+                <CardMedia src={} sx={{ display:{xs:"none",md:"block"} }} alt="Website & Design" />
             </Box>
             <Box sx={{width:{xs:"100%",sm:"50%",md:"25%"}}}>
-                <CardMedia src={Webdesign} sx={{ display:{xs:"none",md:"block"}}} alt="Seo" />
+                <CardMedia src={} sx={{ display:{xs:"none",md:"block"}}} alt="Seo" />
             </Box>
             <Box sx={{width:{xs:"100%",sm:"50%",md:"25%"}}}>
-                <CardMedia src={Webdesign} sx={{ display:{xs:"none",md:"block"}}}alt="E-Commerce Development" />
+                <CardMedia src={} sx={{ display:{xs:"none",md:"block"}}}alt="E-Commerce Development" />
             </Box>
             <Box sx={{width:{xs:"100%",sm:"50%",md:"25%"}}}>
-                <CardMedia src={Webdesign} sx={{ display:{xs:"none",md:"block"}}} alt="Socisl Media" />
+                <CardMedia src={} sx={{ display:{xs:"none",md:"block"}}} alt="Socisl Media" />
             </Box>
         </Box>
     )
