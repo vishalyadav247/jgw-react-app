e129aa9ea0b2d59e54688eaa5e6705f2a49cd504
vishal <vishal@justgoweb.com>
2023-04-21 15:00:49 +0530

    shopify page development

--- src/blogs.js
+++ src/blogs.js
@@ -1,23 +1,34 @@
-import { Box, Button } from '@mui/material'
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+// import { Box, Button } from '@mui/material'
+// import axios from 'axios';
+// import React, { useEffect, useState } from 'react'
+
+// export default function Blogs() {
+//     const [data,setData]=useState([]);
+//     useEffect(()=>{
+//         axios.get('http://localhost:7000/blog/getAllBlogDetails').then((res)=>{
+//         setData(JSON.parse(res.data.data));
+//     })
+//     },[])   
+
+//   return (
+//     <>
+//       <div style={{backgroundColor:'black',height:'84px',width:'100%',position:'sticky',top:'0px'}}></div>
+//         <Box sx={{height:'100vh'}}>
+//          {data.map((item)=>{
+//             console.log(item.description);
+//          })}
+//         </Box>
+//     </>
+//   )
+// }
+
+import React from 'react'
 
 export default function Blogs() {
-    const [data,setData]=useState([]);
-    useEffect(()=>{
-        axios.get('http://localhost:7000/blog/getAllBlogDetails').then((res)=>{
-        setData(JSON.parse(res.data.data));
-    })
-    },[])   
-
   return (
-    <>
-      <div style={{backgroundColor:'black',height:'84px',width:'100%',position:'sticky',top:'0px'}}></div>
-        <Box sx={{height:'100vh'}}>
-         {data.map((item)=>{
-            console.log(item.description);
-         })}
-        </Box>
-    </>
+    <div>
+      Blogs
+    </div>
   )
 }
+
--- src/components/LetsTalkFormBanner.js
+++ src/components/LetsTalkFormBanner.js
@@ -9,15 +9,15 @@ import Button from '@mui/material/Button';
 export default function LetsTalkFormBanner(props) {
     const obj = {
         maxWidth: "1920px",
+        margin:'auto',
         backgroundSize: "cover !important",
         background: `url(${props.banner})`,
         backgroundPosition: "center !important",
-        paddingTop:'102px',
-        paddingBottom:'50px'
+        padding:{xs:'30px 20px 80px 20px',md:'117px 20px 50px 20px'}
     }
     const titleCss = {
         color: '#ffffff',
-        fontSize: '37px',
+        fontSize: {xs:'32px',md:'37px'},
         fontFamily: 'open sans',
         fontWeight: '700',
         marginTop: '65px',
@@ -40,19 +40,35 @@ export default function LetsTalkFormBanner(props) {
         letterSpacing: '0.5px',
         color: '#000000'
     }
+    const FormTitleCss = {
+        fontSize:'25px',
+        fontFamily:'open sans',
+        textAlign:'center',
+        color:'#00438b',
+        fontWeight:'600',
+        padding:'32px 0px 17px 0px',
+        backgroundColor:'#ffffff'
+    }
+    const InputWrapperCss = { 
+        margin: '20px 0px 0px 0px', 
+        display: 'grid',
+        padding:'0px 20px 20px 20px',
+        gridGap:'15px' 
+    }
+
     return (
         <>
         <Box className="homeBanner" sx={obj}>
-            <Box sx={{ maxWidth: '1200px', margin: 'auto', height: "100%", display: 'flex' }}>
-                <Box sx={{ width: '61.5%',paddingRight:'50px' }}>
+            <Box sx={{ maxWidth: '1200px', margin: 'auto', height: "100%", display: 'flex',flexDirection:{xs:'column-Reverse',md:'row'} }}>
+                <Box sx={{ width: {xs:'100%',md:'61.5%'},paddingRight:{xs:'0px',md:'50px'} }}>
                     <Typography sx={titleCss}>{props.title}</Typography>
                     <Typography sx={descCss}>{props.description}</Typography>
                     <Link to={props.linkAddress} style={linkCss}>{props.buttonText}</Link>
                 </Box>
-                <Box sx={{ width: '38.5%' ,backgroundColor:'#ffffff',borderRadius:'12% 0px 0px 0px',overflow:'hidden'}}>
+                <Box sx={{ width: {xs:'100%',md:'38.5%'} ,backgroundColor:'#ffffff',borderRadius:{xs:'0px',md:'12% 0px 0px 0px'},overflow:'hidden'}}>
                     <Box sx={{backgroundColor:'#ededed'}}>
-                        <Typography sx={{fontSize:'25px',fontFamily:'open sans',textAlign:'center',color:'#00438b',fontWeight:'600',padding:'32px 0px 17px 0px',backgroundColor:'#ffffff'}}>Let's talk Shopify Plus</Typography>
-                        <Box sx={{ margin: '20px 0px 0px 0px', display: 'grid',padding:'0px 20px 20px 20px',gridGap:'15px' }} className='contactUpperInputWrapper'>
+                        <Typography sx={FormTitleCss}>Let's talk Shopify Plus</Typography>
+                        <Box sx={InputWrapperCss} className='contactUpperInputWrapper'>
                             <TextField className='letsTalkFields' label="Full Name" variant="filled"/>
                             <TextField className='letsTalkFields' label="Email Address" variant="filled" />
                             <TextField className='letsTalkFields' label="Phone Number" variant="filled" />
