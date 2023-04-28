0520e83fb349e449d49c38a460a1cedb2a887348
vishal <vishal@justgoweb.com>
2023-04-28 19:25:06 +0530

    blog page development

--- src/blogs.js
+++ src/blogs.js
@@ -2,27 +2,79 @@ import { Box, Typography, Button, Divider } from '@mui/material'
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
+import moment from 'moment';
 
 export default function Blogs() {
+
   const [data, setData] = useState([]);
+  const [categories, setCategories] = useState({});
+  const [tagId, setTagId] = useState('');
+
   useEffect(() => {
-    async function my() {
-      await axios.get('https://www.justgoweb.com/wp-json/wp/v2/posts').then((response) => {
-        const posts = response.data;
-        setData(posts);
+    function my() {
+      const p1 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(axios.get('https://www.justgoweb.com/wp-json/wp/v2/posts').then((response) => {
+            const posts = response.data;
+            setData(posts);
+          }));
+        }, 100);
+      });
+      const p2 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(axios.get('https://www.justgoweb.com/wp-json/wp/v2/categories').then((response) => {
+            const cat = response.data;
+            const catList = {}
+            for (let b of cat) {
+              catList[b.id] = b.name;
+            }
+            setCategories(catList)
+          }, 100)
+          )
+        })
       })
+      const p3 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(
+            axios.get(`https://www.justgoweb.com/wp-json/wp/v2/tags`).then((response) => {
+              const cat = response.data;
+              const tagList = {}
+              for (let b of cat) {
+                tagList[b.id] = b.name;
+              }
+              for (let b in tagList) {
+                if (tagList[b] == 'popular') {
+                  setTagId(b);
+                  break
+                }
+              }
+            })
+
+          )
+        }, 100)
+      })
+      // p3.then(
+      //   axios.get(`https://www.justgoweb.com/wp-json/wp/v2/posts?tags=${tagId}`).then((response) => {
+      //     const cat = response.data;
+      //     console.log(cat);
+      //   })
+      // )
+     
+
+      Promise.all([p1, p2, p3]).then((values) => {
+        // console.log(values); 
+      });
     }
     my()
-  },[])
+  }, [])
 
 
 
   const wrapperCss = {
     display: "grid",
-    gridTemplateColumns: { xs: "1fr", sm: "1fr 1fr", md: "1fr 1fr 1fr" },
+    gridTemplateColumns: { xs: "1fr", sm: "1fr 1fr", md: "1fr 1fr" },
     gridGap: "40px",
-    maxWidth: "1200px",
-    margin: "auto"
+    width: '67%'
   }
   const itemCss = {
     borderRadius: "10px",
@@ -34,10 +86,8 @@ export default function Blogs() {
     fontSize: "24px",
     fontWeight: "400",
     lineHeight: "30px",
-    // height: { xs: "auto", md: "153px", lg: "auto" },
     height: '90px',
     overflow: 'hidden',
-    // overflow: { xs: "visible", md: "hidden", lg: "visible" }
   }
   const MiddleSpan = {
     borderLeft: "1px solid #ffffffa5",
@@ -55,47 +105,50 @@ export default function Blogs() {
     padding: "10px 20px"
   }
   const loaderCss = {
-    width:'100%',
-    height:'90vh',
-    display:'flex',
-    justifyContent:'center',
-    alignItems:'center',
-    color:'#272bff',
-    fontSize:'40px'
-}
+    width: '100%',
+    height: '90vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: '#272bff',
+    fontSize: '40px'
+  }
 
   return (
     <>
       <div style={{ backgroundColor: 'black', height: '84px', width: '100%', position: 'sticky', top: '0px' }}></div>
       {data[0] ? (
-      <Box sx={{ backgroundColor: '#f8f8f8', padding: { xs: '60px 20px', md: '60px 30px', lg: '60px 0px' } }}>
-        <Box className="blogsWrapper" sx={wrapperCss}>
-        
-          {data.map(post => (
-            <Box className="blogItem" sx={itemCss} key={post.id}>
-              <Box className='blogImageWrapper'>
-                <img src={post.yoast_head_json.og_image[0].url} alt="blogImage" />
-              </Box>
-              <Box className='blogContent' sx={{ backgroundColor: "rgba(0,67,139,1)", padding: "32px 25px", marginTop: "-6px" }}>
-                <Typography className='blogTitle' sx={BlogTitle}>{post.yoast_head_json.og_title}</Typography>
+        <Box sx={{ backgroundColor: '#f8f8f8', padding: { xs: '60px 20px', md: '60px 30px', lg: '100px 0px' } }}>
 
-                <Divider sx={{ width: "100%", height: "1px", backgroundColor: "white", margin: "25px 0px" }} />
-                <Typography sx={{ color: '#ffffff', fontSize: '12px', fontFamily: 'open sans' }}>
-                  <span>By {post.yoast_head_json.twitter_misc['Written by']}</span>
-                  <span style={MiddleSpan}>December 28th, 2020</span>
-                  <span>Categories: Shopify, Web Development</span>
-                </Typography>
-                <Button variant="contained" sx={BlogCardReadMoreBtn} ><Link to={`/posts/${post.id}`}>Read More</Link></Button>
-              </Box>
+          <Box sx={{ maxWidth: "1200px", margin: "auto", display: 'flex' }}>
+            <Box className="blogsWrapper" sx={wrapperCss}>
+              {data.map(post => (
+                <Box className="blogItem" sx={itemCss} key={post.id}>
+                  <Box className='blogImageWrapper'>
+                    <img src={post.yoast_head_json.og_image[0].url} alt="blogImage" />
+                  </Box>
+                  <Box className='blogContent' sx={{ backgroundColor: "rgba(0,67,139,1)", padding: "32px 25px", marginTop: "-6px" }}>
+                    <Typography className='blogTitle' sx={BlogTitle} dangerouslySetInnerHTML={{ __html: post.title.rendered }} />
+                    <Divider sx={{ width: "100%", height: "1px", backgroundColor: "white", margin: "25px 0px" }} />
+                    <Typography sx={{ color: '#ffffff', fontSize: '12px', fontFamily: 'open sans' }}>
+                      <span>By {post.yoast_head_json.twitter_misc['Written by']}</span>
+                      <span style={MiddleSpan}>{moment(post.date).format('MMMM Do , YYYY')}</span>
+                      <span>categories : {categories[post.categories[0]]} {post.categories[1] ? ", " + categories[post.categories[1]] : ""}</span>
+                    </Typography>
+                    <Button variant="contained" sx={BlogCardReadMoreBtn} ><Link to={`/posts/${post.id}`} style={{ color: '#ffffff', textDecoration: 'none' }}>Read More</Link></Button>
+                  </Box>
+                </Box>
+              ))}
             </Box>
-          ))}
-          
+            <Box sx={{ width: '33%', padding: '0px 0px 0px 50px' }}>
+              <Typography>{tagId}</Typography>
+              <Typography sx={{ backgroundColor: '#f5881f', padding: '8px 11px 10px 11px', color: '#fff', fontSize: '17px', fontWeight: '800', fontFamily: 'poppins' }}>Popular</Typography>
+            </Box>
+          </Box>
 
         </Box>
-
-      </Box>
-      ) 
-          : <Box sx={loaderCss}>Loading...</Box>}
+      )
+        : <Box sx={loaderCss}>Loading...</Box>}
 
     </>
   )
--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,45 +1,93 @@
 import React from 'react';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { Typography,Box} from '@mui/material';
+import { Typography, Box } from '@mui/material';
+import moment from 'moment';
 
 
 export default function SinglePost() {
-    const postId = window.location.pathname
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        async function my() {
-          await axios.get(`https://www.justgoweb.com/wp-json/wp/v2${postId}`).then((response) => {
+  const postId = window.location.pathname;
+  const [data, setData] = useState([]);
+  const [categories, setCategories] = useState({});
+
+
+  useEffect(() => {
+
+    function my() {
+      const p1 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(axios.get(`https://www.justgoweb.com/wp-json/wp/v2${postId}`).then((response) => {
             const posts = response.data;
+            console.log(posts);
+            // console.log(posts.yoast_head_json.schema['@graph'][4].description);
             setData(posts);
-          })
-        }
-        my()
-      }, [])
-      const loaderCss = {
-        width:'100%',
-        height:'90vh',
-        display:'flex',
-        justifyContent:'center',
-        alignItems:'center',
-        color:'#272bff',
-        fontSize:'40px'
+          }));
+        }, 100);
+      });
+
+      const p2 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(axios.get('https://www.justgoweb.com/wp-json/wp/v2/categories').then((response) => {
+            const cat = response.data;
+            const catList = {}
+            for (let b of cat) {
+              catList[b.id] = b.name;
+            }
+            setCategories(catList)
+          }, 100)
+          )
+        })
+      })
+
+      Promise.all([p1, p2]).then((values) => {
+        // console.log(values); 
+      });
     }
+    my()
 
-    return (
-        <>
-            <div style={{ backgroundColor: 'black', height: '84px', width: '100%', position: 'sticky', top: '0px' }}></div>
-            {data.id ? (
-            <Box sx={{maxWidth:'800px',margin:'auto',padding:'50px 0px',overflow:'hidden'}}>
-            <Typography>{data.yoast_head_json.og_title}</Typography>
+  }, [])
+
+  const loaderCss = {
+    width: '100%',
+    height: '90vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: '#272bff',
+    fontSize: '40px'
+  }
+  const titleCss={
+    fontSize:'32px',
+    marginBottom:'30px',
+    fontFamily:'poppins'
+  }
+
+  return (
+    <>
+      <div style={{ backgroundColor: 'black', height: '84px', width: '100%', position: 'sticky', top: '0px' }}></div>
+      {data.id ? (
+        <Box sx={{ maxWidth: '1200px', margin: 'auto', padding: '50px 0px', overflow: 'hidden' }}>
+          <Box sx={{ maxWidth: '800px' }}>
+            <Typography sx={titleCss} dangerouslySetInnerHTML={{ __html: data.title.rendered }} />
             <img src={data.yoast_head_json.og_image[0].url} alt="blogImage" />
-            <Typography>{data.content.rendered}</Typography>
+            <Typography sx={{fontFamily:"open sans",fontSize:'17px'}} dangerouslySetInnerHTML={{ __html: data.content.rendered }} />
+            <Typography sx={{margin:'70px 0px 20px 0px',borderBottom:'1px solid rgb(252, 176, 64)',borderTop:'1px solid rgb(252, 176, 64)',padding:'10px 0px'}}>
+                    <span>{moment(data.date).format('MMMM Do , YYYY')}</span>
+                    <span> | {categories[data.categories[0]]} {data.categories[1] ? ", " + categories[data.categories[1]] : ""}</span>
+                  </Typography>
+            <Typography sx={{fontFamily:'poppins',fontSize:'20px',color:'#00438b'}}>About the Author: {data.yoast_head_json.schema['@graph'][4].name}</Typography>
+            <Box sx={{display:'flex',margin:'30px 0px 20px 0px' }}>
+              <img src={data.yoast_head_json.schema['@graph'][4].image['url']} alt="blogImage" style={{  width: '90px' }}/>
+              <Typography sx={{padding:'0px 0px 0px 20px'}}>{data.yoast_head_json.schema['@graph'][4].description}</Typography>
             </Box>
-            ) 
-            : <Box sx={loaderCss}>Loading...</Box>}
+            
+          </Box>
+        </Box>
+      )
+        : <Box sx={loaderCss}>Loading...</Box>}
 
 
 
-        </>
-    )
+    </>
+  )
 }
